119d17ba05e080fe4966a30d24e8e61900645639
Pinchas Friedman <philipmfriedman@gmail.com>
2012-03-22 21:26:14 -0400

    Test ActionFilterAttribute

---
+++ KidSteps/KidSteps/ActionFilters/TestActionFilterAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KidSteps.ActionFilters
+{
+    public class TestActionFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {            
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
